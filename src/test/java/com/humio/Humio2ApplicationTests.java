3c43a6e92e96d3798da1c0b6d6b69a39ba458371
Rod Johnson <johnsonroda@gmail.com>
2018-03-19 20:06:34 +1100

    Initial commit from Atomist

---
+++ src/test/java/com/humio/Humio2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.humio;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Humio2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
